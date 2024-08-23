7d4748c3ae6a1cb6275378e3b1b53c590cdf6fb2
Alysxn <alysonsouza258il@gmail.com>
2024-08-23 10:41:46 -0400

    all files

---
+++ src/App.js
@@ -0,0 +1,21 @@
+function App() {
+  return (
+    <div className="container">
+      <h1 className="title">Buscador CEP</h1>
+
+      <div className="containerInput">
+      </div>
+      <input type="text" 
+      placeholder="Digite seu cep..."
+      />
+
+      <button className="buttonSearch">
+        Procurar
+      </button>
+      
+
+    </div>
+  );
+}
+
+export default App;
